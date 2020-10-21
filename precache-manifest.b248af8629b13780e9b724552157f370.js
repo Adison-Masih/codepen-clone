0f42d585041c4aeb5244c7bbb42ef38aab97f22b
Adison Masih <adicodetech@gmail.com>
2020-10-21 10:11:36 +0530

    Updates

---
+++ precache-manifest.b248af8629b13780e9b724552157f370.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1de7de438c1218a72a0ff1643734c5",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "331c317392f7f0964d70",
+    "url": "/codepen-clone/static/css/2.61344d53.chunk.css"
+  },
+  {
+    "revision": "3f0d487ab8b5cd1ec518",
+    "url": "/codepen-clone/static/css/main.dbd319f6.chunk.css"
+  },
+  {
+    "revision": "331c317392f7f0964d70",
+    "url": "/codepen-clone/static/js/2.b909a3b0.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/codepen-clone/static/js/2.b909a3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f0d487ab8b5cd1ec518",
+    "url": "/codepen-clone/static/js/main.121431b7.chunk.js"
+  },
+  {
+    "revision": "7e95b33d3c3f8b32c9c4",
+    "url": "/codepen-clone/static/js/runtime-main.3a6f25c4.js"
+  }
+]);
